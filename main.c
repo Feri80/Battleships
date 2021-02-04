c39c279b6b08328b7b11448333e8f408808929c9
Feri80 <bigfarhad7780@gmail.com>
2021-02-04 10:48:16 +0330

    init main

---
+++ main.c
@@ -0,0 +1,28 @@
+//               In The Name Of God
+//                   BattleShips
+//    Fundamentals Of Programming Fianl Project
+//             Created By Farhad Aman
+//                   Winter 2021
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <assert.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdarg.h>
+
+typedef long long ll;
+
+const int mod=1e9+7;
+const ll inf=1000000000000000000L;
+const double eps=1e-7;
+const double pi=3.14159265359;
+
+
+
+int main()
+{
+
+}
